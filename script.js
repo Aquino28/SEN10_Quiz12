4550cd9839cb7dafd0f5f24133abef7e5f57876e
Aquino28 <aquino.edu2028@gmail.com>
2025-10-29 11:06:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // intentionally missing semicolon to trigger ESLint "semi" rule
 document.addEventListener('DOMContentLoaded', function() {
-  const p = document.createElement('p')
-  p.textContent = "Page loaded — this JS file is intentionally missing a semicolon to fail the linter"
-  document.querySelector('main').appendChild(p)
+  const p = document.createElement('p');
+  p.textContent = "Page loaded — this JS file is intentionally missing a semicolon to fail the linter";
+  document.querySelector('main').appendChild(p);
 });
+
 
 
 
