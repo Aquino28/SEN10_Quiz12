48d629d871e81b4b73a3bd81d6756693c287d4f5
Aquino28 <aquino.edu2028@gmail.com>
2025-10-29 11:04:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const p = document.createElement('p')
   p.textContent = "Page loaded — this JS file is intentionally missing a semicolon to fail the linter"
   document.querySelector('main').appendChild(p)
-})
+});
+
 
 
 
