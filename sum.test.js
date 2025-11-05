b874b502ef9219c46dbc3df2d36e5a50606750b2
Aquino28 <aquino.edu2028@gmail.com>
2025-11-05 11:20:26 +0800

    Update sum.test.js test pass

--- sum.test.js
+++ sum.test.js
@@ -5,6 +5,6 @@ test("adds 1 + 2 to equal 3", () => {
 });
 
 
-test("adds 2 + 2 to equal 5", () => {
-  expect(sum(2, 2)).toBe(5);
+test("adds 2 + 2 to equal 4", () => {
+  expect(sum(2, 2)).toBe(4);
 });
